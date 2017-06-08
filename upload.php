5dfaee231b52c5f7d7f6a1e6994ecad395bc55a1
Rishabh Dev Manu <rishabhdevmanu@gmail.com>
2017-06-08 12:53:53 +0530

    Create upload.php
    some patches

--- upload.php
+++ upload.php
@@ -1,60 +1,41 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <br>
-    <input type="submit" value="Upload Image" name="submit">
-</form>
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    }
-    else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-}
-else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    }
-    else {
-        echo "Sorry, there was an error uploading your file.";
-    }
+if(isset($_POST["submit"]))
+{
+  $f_name = $_FILES["myfile"]["name"];
+	$f_tmp = $_FILES["myfile"]["tmp_name"];
+  $f_size= $_FILES["myfile"]["size"];
+  $f_extension= explode(".","$f_name");
+  $f_extension=strtolower(end($f_extension));
+  $f_newfile=uniqid().".".$f_extension;
+  $store = "uploads/".$f_newfile;
+  if($f_extension=="jpg"||$f_extension=="gif"||$f_extension="png")
+  {
+	   if($f_size>=1000000000000000000)
+	    {
+		      echo "Max size exceeded";
+	       }
+	    else
+	     {
+		       if(move_uploaded_file($f_tmp,$store))
+		         {
+			            echo "Your file has been uploaded";
+		              }
+        }
+	 }
+	 else
+	 {
+		 echo "File extension is not correct";
+   }
 }
 ?>
-
+<html>
+<head>
+<title>File_Uploading</title>
+</head>
+<body>
+<form action="" method="post" enctype="multipart/form-data">
+<p><input type="file" name="myfile"/></p>
+<p><input type="submit" value="upload" name="submit" /></p>
+</form>
 </body>
 </html>
