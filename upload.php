166b8c7b3c643f86c7658fd04c0411f926a3233c
Rishabh Dev Manu <rishabhdevmanu@gmail.com>
2017-06-08 11:16:18 +0530

    Create upload.php

--- upload.php
+++ upload.php
@@ -19,7 +19,8 @@ if(isset($_POST["submit"])) {
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
-    } else {
+    }
+    else {
         echo "File is not an image.";
         $uploadOk = 0;
     }
@@ -44,10 +45,12 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
-} else {
+}
+else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
+    }
+    else {
         echo "Sorry, there was an error uploading your file.";
     }
 }
